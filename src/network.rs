063b66c5a8cf983a3c0a443047cf4c5a607900b6
Ali Mousa <amousa11@gmail.com>
2018-08-10 16:57:01 -0700

    added Network enum

---
+++ src/network.rs
@@ -0,0 +1,19 @@
+use std::fmt;
+
+/// The Network enum represents the different types of Networks we can create BitcoinWallets for
+#[derive(Debug, PartialEq, Eq)]
+pub enum Network {
+    Mainnet,
+    Testnet,
+    Error
+}
+
+impl fmt::Display for Network {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Network::Mainnet => write!(f, "Mainnet"),
+            Network::Testnet => write!(f, "Testnet"),
+            _ => write!(f, "Error"),
+        }
+    }
+}
