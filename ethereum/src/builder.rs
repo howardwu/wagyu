9dc172963050da328c1d16109cc1c63e8cd1a051
raychu86 <raychu86@gmail.com>
2018-08-25 16:44:15 -0700

    Implemented Ethereum Wallet helpers

---
+++ ethereum/src/builder.rs
@@ -0,0 +1,30 @@
+use wallet::EthereumWallet;
+
+/// A WalletBuilder helps to construct a EthereumWallet
+#[derive(Default)]
+pub struct WalletBuilder {
+    testnet: bool,
+}
+
+impl WalletBuilder {
+    pub fn new() -> WalletBuilder {
+        WalletBuilder {
+            testnet: false,
+        }
+    }
+
+    /// Construct an EthereumWallet
+    pub fn build() -> EthereumWallet {
+        EthereumWallet::new()
+    }
+
+    pub fn build_many_from_options(
+        count: usize,
+    ) -> Vec<EthereumWallet> {
+        let mut wallets = Vec::with_capacity(count);
+        for _ in 0..count {
+            wallets.push(WalletBuilder::build());
+        }
+        wallets
+    }
+}
