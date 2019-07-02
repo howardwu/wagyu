fa5c3347db3db571a0275f41ce1407d744489707
Howard Wu <howardwu@Computer-2.local>
2019-07-02 11:21:39 -0700

    Update monero network to convention

--- monero/src/network.rs
+++ monero/src/network.rs
@@ -4,10 +4,9 @@ use std::fmt;
 /// The Network enum represents the different types of Networks we can create MoneroWallets for.
 #[derive(Serialize, Debug, PartialEq, Eq, Clone)]
 pub enum Network {
-    /// Monero Mainnet
     Mainnet,
-    /// Monero Testnet
-    Testnet
+    Testnet,
+    Error
 }
 
 /// Returns the prefix for a given network
@@ -15,6 +14,7 @@ pub fn get_prefix(network: &Network) -> Option<&'static [u8]> {
     match network {
         Network::Testnet => Some(&[0x35]),
         Network::Mainnet => Some(&[0x12]),
+        _ => None
     }
 }
 
