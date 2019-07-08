a67ad5a99f3775a499b84b32913bad57cae435aa
raychu86 <raychu86@gmail.com>
2019-07-08 12:34:32 -0700

    cleanup signature generation

--- ethereum/src/transaction.rs
+++ ethereum/src/transaction.rs
@@ -16,7 +16,7 @@ pub struct Transaction {
     pub data: Vec<u8>
 }
 
-pub struct TransactionSignature {
+struct TransactionSignature {
     v: Vec<u8>,
     r: Vec<u8>,
     s: Vec<u8>
@@ -25,19 +25,19 @@ pub struct TransactionSignature {
 impl Transaction {
 
     // Generates a raw Ethereum transaction
-    fn new(nonce: &str, gas_price: &str, gas: &str, to: &str, value: &str, data: &str) -> Self {
-        let nonce = Self::str_to_U256(nonce);
-        let gas_price = Self::str_to_U256(gas_price);
-        let gas = Self::str_to_U256(gas);
+    pub fn new(nonce: &str, gas_price: &str, gas: &str, to: &str, value: &str, data: &str) -> Self {
+        let nonce = Self::str_to_u256(nonce);
+        let gas_price = Self::str_to_u256(gas_price);
+        let gas = Self::str_to_u256(gas);
         let to = hex::decode(to).unwrap();
-        let value = Self::str_to_U256(value);
+        let value = Self::str_to_u256(value);
         let data = data.as_bytes().to_vec();
 
         Self { nonce, gas_price, gas, to, value, data}
     }
 
     // Create a Recursive Length Prefix from the raw transaction
-    fn encode_transaction_rlp (&self, stream: &mut RlpStream) {
+    pub fn encode_transaction_rlp (&self, stream: &mut RlpStream) {
         //Encode individual transaction attributes into RLP Type
         stream.append(&self.nonce);
         stream.append(&self.gas_price);
@@ -61,7 +61,7 @@ impl Transaction {
 
     pub fn sign_transaction(&self, private_key: &str, chain_id: u8) -> Vec<u8> {
         let hash = self.raw_transaction_hash(chain_id);
-        let signature = ecdsa_sign(&hash, private_key, &chain_id);
+        let signature = Self::ecdsa_sign(&hash, private_key, &chain_id);
         let mut signed_transaction = RlpStream::new();
         signed_transaction.begin_unbounded_list();
         self.encode_transaction_rlp(&mut signed_transaction);
@@ -73,21 +73,21 @@ impl Transaction {
         signed_transaction.as_raw().to_vec()
     }
 
-    pub fn str_to_U256(s: &str) -> U256 {
+    pub fn str_to_u256(s: &str) -> U256 {
         U256::from_dec_str(s).unwrap()
     }
-}
 
-pub fn ecdsa_sign(hash: &[u8], private_key_wif: &str, chain_id: &u8) -> TransactionSignature {
-    let s = Secp256k1::signing_only();
-    let msg = secp256k1::Message::from_slice(hash).unwrap();
-    let ethPrivateKey = EthereumPrivateKey::from_wif(private_key_wif).unwrap();
-    let key = ethPrivateKey.secret_key;
-    let (v, sig_bytes) = s.sign_recoverable(&msg, &key).serialize_compact(&s);
+    fn ecdsa_sign(hash: &[u8], private_key: &str, chain_id: &u8) -> TransactionSignature {
+        let s = Secp256k1::signing_only();
+        let msg = secp256k1::Message::from_slice(hash).unwrap();
+        let ethereum_private_key = EthereumPrivateKey::from(private_key).unwrap();
+        let key = ethereum_private_key.secret_key;
+        let (v, sig_bytes) = s.sign_recoverable(&msg, &key).serialize_compact(&s);
 
-    TransactionSignature {
-        v: vec![v.to_i32() as u8 + chain_id * 2 + 35],
-        r: sig_bytes[0..32].to_vec(),
-        s: sig_bytes[32..64].to_vec(),
+        TransactionSignature {
+            v: vec![v.to_i32() as u8 + chain_id * 2 + 35],
+            r: sig_bytes[0..32].to_vec(),
+            s: sig_bytes[32..64].to_vec(),
+        }
     }
 }
