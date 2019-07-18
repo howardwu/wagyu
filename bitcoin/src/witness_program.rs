e32456388294ade96f92b355ae04967d52973ea0
howardwu <howardwu@berkeley.edu>
2019-07-18 15:49:40 -0700

    Update witness program

--- bitcoin/src/witness_program.rs
+++ bitcoin/src/witness_program.rs
@@ -1,49 +1,66 @@
-//!WitnessProgram
-//! This module contains a representation of a Bitcoin witness program and utility functions
+//!
+//! WitnessProgram
+//!
+//! This module contains the representation of a Bitcoin witness program and utility functions
 //! related to the representation of such programs.
+//!
+//! If the version byte is 0, and the witness program is 20 bytes:
+//! - It is interpreted as a pay-to-witness-public-key-hash (P2WPKH) program.
+//! - The witness must consist of exactly 2 items (≤ 520 bytes each). The first one a signature, and the second one a public key.
+//! - The HASH160 of the public key must match the 20-byte witness program.
+//! - After normal script evaluation, the signature is verified against the public key with CHECKSIG operation. The verification must result in a single TRUE on the stack.
+//!
+//! If the version byte is 0, and the witness program is 32 bytes:
+//! - It is interpreted as a pay-to-witness-script-hash (P2WSH) program.
+//! - The witness must consist of an input stack to feed to the script, followed by a serialized script (witnessScript).
+//! - The witnessScript (≤ 10,000 bytes) is popped off the initial witness stack. SHA256 of the witnessScript must match the 32-byte witness program.
+//! - The witnessScript is deserialized, and executed after normal script evaluation with the remaining witness stack (≤ 520 bytes for each stack item).
+//! - The script must not fail, and result in exactly a single TRUE on the stack.
+//!
+//! If the version byte is 0, but the witness program is neither 20 nor 32 bytes, the script must fail.
+//!
+
 use std::fmt;
 use std::str::FromStr;
-use hex::encode;
+use hex;
 
 pub struct WitnessProgram {
+    /// The version byte
     pub version: u8,
+    /// The witness program bytes
     pub program: Vec<u8>,
 }
 
 impl WitnessProgram {
-    /// Creates a new WitnessProgram given a program with a version byte, data size, and data
-    pub fn new(prog_bytes: &[u8]) -> Result<WitnessProgram, &'static str> {
-        if prog_bytes.len() < 2 {
+    /// Returns a new WitnessProgram given a program with a version byte, data size, and data.
+    pub fn new(program: &[u8]) -> Result<WitnessProgram, &'static str> {
+        if program.len() < 2 {
             return Err("Invalid program");
         }
 
-        let mut program: Vec<u8> = Vec::new();
-        program.extend_from_slice(&prog_bytes[2..]);
-
-        // https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#Decoding
-        let program_length = prog_bytes[1];
-
-        if program.len() != program_length as usize {
-            return Err("Mismatched program length");
-        }
-
-        let mut version = prog_bytes[0];
+        let mut version = program[0];
         if version > 0x50 && version <= 0x60 {
             version -= 0x50;
-        }
-        else if version != 0x00 {
+        } else if version != 0x00 {
             return Err("Invalid witness version")
         }
 
-        let new_program = WitnessProgram { version, program };
-        match new_program.validate() {
-            Ok(()) => Ok(new_program),
+        // https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#Decoding
+        let data_size = program[1] as usize;
+        let data = program[2..].to_vec();
+        if data_size != data.len() {
+            return Err("Mismatched program length");
+        }
+
+        let program = WitnessProgram { version, program: data };
+        match program.validate() {
+            Ok(()) => Ok(program),
             Err(e) => Err(e)
         }
     }
 
     pub fn validate(&self) -> Result<(), &'static str> {
-        if self.program.len() > 40 || self.program.len() < 2 {
+        if self.program.len() < 2 || self.program.len() > 40 {
             return Err("Invalid program length");
         }
 
@@ -62,19 +79,16 @@ impl WitnessProgram {
         if version == 0 && !(self.program.len() == 20 || self.program.len() == 32) {
             return Err("Invalid program length for witness version 0");
         }
+
         Ok(())
     }
 
-    /// Returns a byte vector representing a witness program
-    #[allow(dead_code)]
+    /// Returns the witness program as a byte vector.
     pub fn to_vec(&self) -> Vec<u8> {
-        let mut ret: Vec<u8> = Vec::new();
-        let version = WitnessProgram::convert_version(self.version);
-        ret.push(version);
-        ret.push(self.program.len() as u8);
-        ret.extend_from_slice(&self.program);
-
-        ret
+        let mut output = vec![WitnessProgram::convert_version(self.version)];
+        output.push(self.program.len() as u8);
+        output.copy_from_slice(&self.program);
+        output
     }
 
     /// A BIP173 version conversion utility function.
@@ -93,24 +107,18 @@ impl WitnessProgram {
 impl FromStr for WitnessProgram {
     type Err = &'static str;
 
-    /// Converts a hex string into a witness program
+    /// Returns a witness program given its hex representation.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let prog_bytes = match hex::decode(s) {
+        WitnessProgram::new(&match hex::decode(s) {
             Ok(bytes) => bytes,
             Err(_) => return Err("Error decoding hex string")
-        };
-        WitnessProgram::new(&prog_bytes)
+        })
     }
 }
 
 impl fmt::Display for WitnessProgram {
-    /// Prints the hex representation of a witness program
+    /// Prints a witness program in hex representation.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,
-               "{:02x}{:02x}{}",
-               WitnessProgram::convert_version(self.version),
-               self.program.len() as u8,
-               encode(&self.program)
-        )
+        write!(f, "{}", hex::encode(self.to_vec()))
     }
 }
