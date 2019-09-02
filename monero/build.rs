2f82b74c0434da4e74c9d055c11afb15376d3c51
Collin Chin <collin.chin@berkeley.edu>
2019-09-02 14:05:43 -0700

    add build config for macos

--- monero/build.rs
+++ monero/build.rs
@@ -2,12 +2,16 @@ use::cmake::Config;
 
 fn main() {
 //  dynamically link to standard c++ lib so we can call boost functions in mymonero-core-cpp
-//    println!("cargo:rustc-link-lib=dylib=stdc++"); // Linux/GCC
-    println!("cargo:rustc-link-lib=dylib=c++");
 
-    // link boost libraries (tested on OSX only!)
+    if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-lib=dylib=c++");
+        println!("cargo:rustc-link-lib=boost_thread-mt");
+    } else {
+        println!("cargo:rustc-link-lib=dylib=stdc++"); // Linux/GCC
+        println!("cargo:rustc-link-lib=boost_thread");
+    }
+
     println!("cargo:rustc-link-lib=boost_system");
-    println!("cargo:rustc-link-lib=boost_thread-mt");
 
     // build mymonero-core-cpp
     let mymonero_path = Config::new("mymonero-core-cpp").build();
