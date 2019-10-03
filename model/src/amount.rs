9b96c51e70b998d9e05ede2fdd97d6e8c222a13d
raychu86 <raychu86@gmail.com>
2019-10-02 19:08:31 -0700

    added amount trait

---
+++ model/src/amount.rs
@@ -0,0 +1,16 @@
+use std::{
+    fmt::{Debug, Display},
+    hash::Hash,
+};
+
+/// The interface for a generic amount.
+pub trait Amount: Clone + Debug + Display + Send + Sync + 'static + Eq + Ord + Sized + Hash {}
+
+#[derive(Debug, Fail)]
+pub enum AmountError {
+    #[fail(display = "the amount: {} exceeds the supply bounds of {}", _0, _1)]
+    AmountOutOfBounds(String, String),
+
+    #[fail(display = "invalid amount: {}", _0)]
+    InvalidAmount(String),
+}
