c2e5ef7ee1f630e669746d5a03c382d74dc5ae4e
raychu86 <raychu86@gmail.com>
2019-10-03 17:49:25 -0700

    added ethereum amounts

--- model/src/amount.rs
+++ model/src/amount.rs
@@ -11,6 +11,9 @@ pub enum AmountError {
     #[fail(display = "the amount: {} exceeds the supply bounds of {}", _0, _1)]
     AmountOutOfBounds(String, String),
 
+    #[fail(display = "{}: {}", _0, _1)]
+    Crate(&'static str, String),
+
     #[fail(display = "invalid amount: {}", _0)]
     InvalidAmount(String),
 }
