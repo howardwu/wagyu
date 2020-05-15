608c27d713b01e6aa5dc940768fec275de02dea6
Andrew Breckenridge <asbreckenridge@me.com>
2020-05-15 01:52:55 -0700

    remove the only warning in the project

--- zcash/src/librustzcash/mod.rs
+++ zcash/src/librustzcash/mod.rs
@@ -8,5 +8,5 @@ pub mod zip32;
 use crate::librustzcash::sapling_crypto::jubjub::JubjubBls12;
 
 lazy_static! {
-    pub static ref JUBJUB: JubjubBls12 = { JubjubBls12::new() };
+    pub static ref JUBJUB: JubjubBls12 = JubjubBls12::new();
 }
